50f087669506f5da237d36228375b932884c3d28
faaip <frederikjuutilainen@gmail.com>
2020-02-20 11:00:46 +0100

    error in dependencies install script

--- run.py
+++ run.py
@@ -10,13 +10,16 @@ import os
 
 # input arg parsing
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--fullscreen', help='Display window in full screen', action='store_true')
-parser.add_argument('-d', '--debug', help='Display debug info', action='store_true')
+parser.add_argument('-f', '--fullscreen',
+                    help='Display window in full screen', action='store_true')
+parser.add_argument(
+    '-d', '--debug', help='Display debug info', action='store_true')
 args = parser.parse_args()
 
 model = Sequential()
 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu', input_shape=(48, 48, 1)))
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -38,7 +41,34 @@ model.load_weights('model.h5')
 cv2.ocl.setUseOpenCL(False)
 
 # dictionary which assigns each label an emotion (alphabetical order)
-emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful",
+                3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+
+def draw_border(img, pt1, pt2, color=(255, 0, 0), thickness=8, r=15, d=10):
+    x1, y1 = pt1
+    x2, y2 = pt2
+
+    # Top left
+    cv2.line(img, (x1 + r, y1), (x1 + r + d, y1), color, thickness)
+    cv2.line(img, (x1, y1 + r), (x1, y1 + r + d), color, thickness)
+    cv2.ellipse(img, (x1 + r, y1 + r), (r, r), 180, 0, 90, color, thickness)
+
+    # Top right
+    cv2.line(img, (x2 - r, y1), (x2 - r - d, y1), color, thickness)
+    cv2.line(img, (x2, y1 + r), (x2, y1 + r + d), color, thickness)
+    cv2.ellipse(img, (x2 - r, y1 + r), (r, r), 270, 0, 90, color, thickness)
+
+    # Bottom left
+    cv2.line(img, (x1 + r, y2), (x1 + r + d, y2), color, thickness)
+    cv2.line(img, (x1, y2 - r), (x1, y2 - r - d), color, thickness)
+    cv2.ellipse(img, (x1 + r, y2 - r), (r, r), 90, 0, 90, color, thickness)
+
+    # Bottom right
+    cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness)
+    cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness)
+    cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness)
+
 
 # start the webcam feed
 cap = cv2.VideoCapture(0)
@@ -52,27 +82,32 @@ while True:
         break
     facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+    faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        # cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        draw_border(frame, (x, y-50), (x+w, y+h+10))
         roi_gray = gray[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        cropped_img = np.expand_dims(np.expand_dims(
+            cv2.resize(roi_gray, (48, 48)), -1), 0)
         prediction = model.predict(cropped_img)
         maxindex = int(np.argmax(prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
     # full screen
     if args.fullscreen:
-        cv2.namedWindow("video", cv2.WND_PROP_FULLSCREEN)          
+        cv2.namedWindow("video", cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty("video", cv2.WND_PROP_FULLSCREEN, 1)
-    
-    # debug info
-    if args.debug:    
-        fps = str(int(1.0 / (time.time() - start_time)))
-        cv2.putText(frame, fps + " fps", (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-    cv2.imshow('video', cv2.resize(frame,(800,480),interpolation = cv2.INTER_CUBIC))
+    # debug info
+    if args.debug:
+        fps = str(int(1.0 / (time.time() - start_time)))
+        cv2.putText(frame, fps + " fps", (20, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow('video', cv2.resize(
+        frame, (800, 480), interpolation=cv2.INTER_CUBIC))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
