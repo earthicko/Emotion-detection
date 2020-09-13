4b6512f87e68d7975d929cc8be7d84064a49e9df
earthicko <earthicko@gmail.com>
2020-09-13 16:17:47 +0900

    added test mode and args

--- grbl.py
+++ grbl.py
@@ -24,32 +24,44 @@ class GRBL:
         self.iteration = iteration
         self.iteration_default = iteration
 
-        self.port = serial.Serial(port, 115200, timeout=timeout)
-        self.port.write(b"\r\n\r\n")
-        time.sleep(1)
-        self.port.reset_output_buffer()  # Flush startup text in serial input
-        self.port.reset_input_buffer()  # Flush startup text in serial input
+        if not args.test:
+            self.port = serial.Serial(port, 115200, timeout=timeout)
+            self.port.write(b"\r\n\r\n")
+            time.sleep(1)
+            self.port.reset_output_buffer()  # Flush startup text in serial input
+            self.port.reset_input_buffer()  # Flush startup text in serial input
+
         if args.verbose:
-            print('GRBL #' + str(num) + ' added: ' + self.port.name)
+            if args.test:
+                print('Dummy GRBL#' + str(num) + ' added')
+            else:
+                print('GRBL #' + str(num) + ' added: ' + self.port.name)
 
     def get_settings(self):
-        if args.verbose:
-            print('GRBL #' + str(self.num) + ' Reading settings from ' + str(self.port.name))
-        self.port.write(b"$$\n")
-        if args.verbose:
-            print(self.port.read(2000).decode('utf-8'), end='')
+        if args.test:
+            print('No settings on test mode')
+        else:
+            if args.verbose:
+                print('GRBL #' + str(self.num) + ' Reading settings from ' + str(self.port.name))
+            self.port.write(b"$$\n")
+            if args.verbose:
+                print(self.port.read(2000).decode('utf-8'), end='')
 
     def set_settings(self, data):
-        if args.verbose:
-            print('GRBL #' + str(self.num) + ' Setting settings to ' + str(self.port.name))
-        for item in data.keys():
+        if args.test:
+            print('No settings on test mode')
+        else:
             if args.verbose:
-                print('Item ' + item + '\tset to ' + str(data[item][1]) + '\tin address ' + data[item][0] + ', echo: ',
-                      end='')
-            msg = data[item][0] + '=' + str(data[item][1]) + '\n'
-            self.port.write(msg.encode())
-            if args.verbose:
-                print(self.port.readline().decode('utf-8'), end='')
+                print('GRBL #' + str(self.num) + ' Setting settings to ' + str(self.port.name))
+            for item in data.keys():
+                if args.verbose:
+                    print('Item ' + item + '\tset to ' + str(data[item][1]) + '\tin address ' + data[item][
+                        0] + ', echo: ',
+                          end='')
+                msg = data[item][0] + '=' + str(data[item][1]) + '\n'
+                self.port.write(msg.encode())
+                if args.verbose:
+                    print(self.port.readline().decode('utf-8'), end='')
 
     def reset(self):
         self.iteration = self.iteration_default
@@ -66,10 +78,10 @@ class GRBL:
         for i in range(3):
             self.position[i] += self.iteration[i]
             if self.position[i] > 50:
-                self.position[i] = 100 - self.position[i]   # 100 - 53 = 47
+                self.position[i] = 100 - self.position[i]  # 100 - 53 = 47
                 self.iteration[i] = - self.iteration[i]
             elif self.position[i] < 50:
-                self.position[i] = - 100 + self.position[i]   # - 100 + 53 = - 47
+                self.position[i] = - 100 + self.position[i]  # - 100 + 53 = - 47
                 self.iteration[i] = - self.iteration[i]
         if args.verbose:
             print('GRBL #' + str(self.num) + ' Iteration')
@@ -77,32 +89,51 @@ class GRBL:
             print(f'After Iteration: {self.iteration[0]}, {self.iteration[1]}, {self.iteration[2]}')
 
     def home(self, wait):
-        if args.verbose:
-            print('GRBL #' + str(self.num) + ' Homing :' + str(self.port.name))
-        self.port.write(b"$H\n")
-        if wait:
-            while self.port.in_waiting <= 0:
-                if args.verbose:
-                    print('GRBL #' + str(self.num) + ' waiting for homing: ' + str(self.port.name))
-                    time.sleep(1)
+        if args.test:
+            print('No homing at test mode')
+        else:
             if args.verbose:
-                print('echo: ' + self.port.readline().decode('utf-8'), end='')
+                print('GRBL #' + str(self.num) + ' Homing :' + str(self.port.name))
+            self.port.write(b"$H\n")
+            if wait:
+                while self.port.in_waiting <= 0:
+                    if args.verbose:
+                        print('GRBL #' + str(self.num) + ' waiting for homing: ' + str(self.port.name))
+                        time.sleep(1)
+                if args.verbose:
+                    print('echo: ' + self.port.readline().decode('utf-8'), end='')
 
     def rx(self):
-        return self.port.readline().decode('utf-8')
+        if args.test:
+            print('No RX at test mode')
+            return 'No RX at test mode'
+        else:
+            return self.port.readline().decode('utf-8')
 
     def tx(self, msg):
-        self.port.write(msg.encode())
+        if args.test:
+            print('No TX at test mode')
+            return 'No TX at test mode'
+        else:
+            self.port.write(msg.encode())
 
     def set_position(self, position, feedrate, mode):
+        for i in range(3):
+            if position[i] > 50 or position[i] < -50:
+                print("ERROR at: GRBL#" + str(self.num) + ": raw position[" + str(i) + "] out of range: " + str(
+                    position[i]))
         if args.verbose:
-            print('GRBL #' + str(self.num) + ' Position Set ' + str(self.port.name))
-            print(f"Data position X: {self.position[0]} Y: {self.position[1]} Z: {self.position[2]}")
+            print(f"Data position X: {position[0]} Y: {position[1]} Z: {position[2]}")
         for i in range(3):
             self.position[i] = map_int(position[i], (-50, 50), (self.pos_min, self.pos_max))
+        for i in range(3):
+            if self.position[i] > self.pos_max or position[i] < self.pos_min:
+                print("ERROR at: GRBL#" + str(self.num) + ": mapped position[" + str(i) + "] out of range: " + str(
+                    self.position[i]))
         self.feedrate = feedrate
         self.mode = mode
         if args.verbose:
+            print('GRBL #' + str(self.num) + ' Position Set ' + str(self.port.name))
             print(f"Mapped position X: {self.position[0]} Y: {self.position[1]} Z: {self.position[2]}")
 
     def move(self):
@@ -115,14 +146,30 @@ class GRBL:
             if args.verbose:
                 print('Output GCode: ' + msg + ', echo: ', end='')
                 print(self.port.readline().decode('utf-8'), end='')
+        else:
+            if args.verbose:
+                print('Output GCode: ' + msg)
 
     def in_waiting(self):
-        return self.port.in_waiting
-    def close(self):
-        self.port.close()
-    def open(self):
-        self.port.open()
+        if args.test:
+            print('No buffer at test mode')
+            return 'No buffer at test mode'
+        else:
+            return self.port.in_waiting
 
+    def close(self):
+        if args.test:
+            print('No serial port at test mode')
+            return 'No serial port at test mode'
+        else:
+            self.port.close()
+
+    def open(self):
+        if args.test:
+            print('No serial port at test mode')
+            return 'No serial port at test mode'
+        else:
+            self.port.open()
 
 
 def map_int(val, src, dst):
